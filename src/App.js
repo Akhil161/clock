64dba6146cda27facd790078c6827dd15ef24221
Akhil <akhilgarg1610@gmail.com>
2022-07-22 13:05:03 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import { PomodoroClock } from './components/PomodoroClock';
+
+function App() {
+  return (
+    <div className="App">
+     <PomodoroClock/>
+    </div>
+  );
+}
+
+export default App;
